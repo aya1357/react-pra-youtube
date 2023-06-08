5fad92b26729b895299ac0e42fdaa2d6aee7b056
aya <katoa5429@gmail.com>
2023-06-08 22:09:43 +0900

    useref

--- react-hooks-yt/react-hooks-tutorial-yt/src/App.jsx
+++ react-hooks-yt/react-hooks-tutorial-yt/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef } from "react";
 import ShincodeContext from "./main.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
   const shincodeInfo = useContext(ShincodeContext);
+  const ref = useRef();
 
   let handleClick = () => {
     setCount(count + 1);
@@ -13,7 +14,12 @@ function App() {
   useEffect(() => {
     console.log("useEffect");
     // setCount(count + 1);
-  }, [count])
+  }, [count]);
+
+  const handleRef = () => {
+    console.log(ref.current.value)
+    console.log(ref.current.offsetHeight)
+  };
 
   return (
     <div className="App">
@@ -25,6 +31,11 @@ function App() {
       <h1>useContext</h1>
       <p>{shincodeInfo.name}</p>
       <p>{shincodeInfo.age}</p>
+
+      <hr />
+      <h1>useRef</h1>
+      <input type="text" ref={ref} />
+      <button onClick={handleRef}>UseRef</button>
     </div>
   );
 }
